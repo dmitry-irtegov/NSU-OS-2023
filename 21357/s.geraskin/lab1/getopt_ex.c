137a6b9a6b58f2c32a16d3d5188a1a4633074b4a
Streyd <Stepan89134685846@yandex.ru>
2023-09-11 10:27:47 +0700

    Testing SSH signing

--- 21357/s.geraskin/lab1/getopt_ex.c
+++ 21357/s.geraskin/lab1/getopt_ex.c
@@ -54,6 +54,7 @@ int main(int argc, char *argv[]){
                 if (errno != 0 || new_U_limit < 0) {
                     fprintf(stderr, "Invalid new ulimit value: %s\n", u_arg);
                 }
+
                 struct rlimit U_limit;
                 if (getrlimit(RLIMIT_FSIZE, &U_limit) == 0) {
                     U_limit.rlim_cur = (rlim_t) new_U_limit;
