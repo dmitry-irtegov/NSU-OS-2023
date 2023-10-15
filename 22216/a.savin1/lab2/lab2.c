9049141e3de79af1a939eee3dc3ae96f4e0e2fe8
Anatoliy Savin <a.savin1@g.nsu.ru>
2023-10-15 22:21:10 +0700

    1

--- 22216/a.savin1/lab2/lab2.c
+++ 22216/a.savin1/lab2/lab2.c
@@ -1,14 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
+#include <sys/types.h>
 
-int main() {
+int main(){
     time_t now;
+    struct tm *sp;
 
-    (void) time( &now );
-    now -= 3600 * 14;
+    putenv("TZ=US/Pacific");
+    
+    (void) time (&now);
 
     printf("%s", ctime( &now ) );
 
-    exit(0);
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    return 0;
 }
