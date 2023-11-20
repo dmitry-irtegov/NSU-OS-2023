f27dd1c2067d794f059e9ff86423df6a4f988223
Solovev-Sergey <55825616+Solovev-Sergey@users.noreply.github.com>
2023-11-20 10:03:21 +0700

    Update time_in_california.c

--- 21345/s.solovev1/lab2/time_in_california.c
+++ 21345/s.solovev1/lab2/time_in_california.c
@@ -8,21 +8,21 @@ extern char *tzname[];
 int main() {
         time_t now;
         struct tm *sp;
-
+        
         putenv("TZ=America/Los_Angeles");
-
+        
         (void) time( &now );
         printf("%s", ctime( &now ) );
         sp = localtime(&now);
-
+        
         printf("%02d/%02d/%02d %d:%02d %s\n",
-            sp->tm_mon + 1,
-            sp->tm_mday,
-            1900 + sp->tm_year,
-            sp->tm_hour,
-            sp->tm_min,
-            tzname[sp->tm_isdst]
+                sp->tm_mon + 1,
+                sp->tm_mday,
+                1900 + sp->tm_year,
+                sp->tm_hour,
+                sp->tm_min,
+                tzname[sp->tm_isdst]
         );
-
-   return 0;
+        
+        return 0;
 }
