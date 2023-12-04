155e0a8e130140be025be3c07932034887eb6ae0
Vova Petrov <v.petrov3@g.nsu.ru>
2023-12-04 12:14:59 +0700

    lab11

---
+++ 22216/v.petrov3/lab11/11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char** environ;
+
+int execvpe(const char* filename, char** argv, char** envp) {
+    char **safe_environ = environ;
+    environ = envp;
+    execvp(filename, argv);
+    environ = safe_environ;
+    return 1;
+}
+
+int main(int argc, char** argv) {
+    char *envp[] = {"HOME=/Users/vovapetrov", "PATH=/usr/bin/", NULL};
+    
+    if (argc >= 2) {
+        if (execvpe(argv[1], argv + 1, envp)) {
+            perror("execvpe failed");
+        }
+    }
+    else {
+        fprintf(stderr, "Bad usage");
+    }
+    return 0;
+}
