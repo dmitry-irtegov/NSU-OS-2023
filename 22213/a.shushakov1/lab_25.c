44df64a4c555813c9f945479caaae1bc011be170
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-12-09 13:01:50 +0700

    Update lab_25.c

--- 22213/a.shushakov1/lab_25.c
+++ 22213/a.shushakov1/lab_25.c
@@ -1,50 +1,52 @@
-#include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
-#define MSGSIZE 20
-
-int main(int argc, char* argv[]) {
+int main() {
     int fildes[2];
-
     if (pipe(fildes) == -1) {
-        perror(argv[0]);
-        exit(1);
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
-
     pid_t pid = fork();
-
-    switch(pid){
-       
-        case(-1):
-            perror("Cannot fork\n");
-            exit(1);
-            break;
-       
-        case(0):
+    switch (pid) {
+        case -1:
+            perror("fork");
             close(fildes[0]);
-            char msgout[] = "I'm tIrEd";
-            if (write(fildes[1], msgout, MSGSIZE) == -1) {
-                perror("Error: Child write\n");
-                close (fildes[1]);           
-            }
-            break;
-       
-        default:
             close(fildes[1]);
-            char msgin[MSGSIZE];
-            if (read(fildes[0], msgin, MSGSIZE) == -1) {
-                perror("Error: Parent read\n");
-                close(fildes[0]);
+            exit(EXIT_FAILURE);
+        case 0: 
+            close(fildes[1]);
+            int in = fildes[0];
+            char ch;
+            while (read(in, &ch, 1)) {
+                printf("%c", tolower(ch));
             }
-            for (int i = 0; i < MSGSIZE - 1; i++) {
-                msgin[i] = toupper(msgin[i]);
-            }
-            printf("Got: %s\n", msgin);
+            printf("\n");
+            close(in);
             break;
+        default:                  
+            close(fildes[0]); 
+            int out = fildes[1];
+            char* message = "I'M TIRED";
+            for (char* msgLen = message; *msgLen != '\0'; msgLen++)
+            {
+                if(write(out, msgLen, 1) == 0) {
+                    perror("write");
+                    close(out);
+                    exit(EXIT_FAILURE);
+                }
+            }
+            close(out);
+
+            if (waitpid(pid, NULL, 0) == -1)
+            {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
     }
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
