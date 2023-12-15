0ec7984a0f77e8b245daf406bd1ca323773b19dd
Solovev-Sergey <55825616+Solovev-Sergey@users.noreply.github.com>
2023-12-15 11:31:56 +0700

    s.solovev1/lab26

---
+++ 21345/s.solovev1/lab26/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX_LEN 256
+
+int main(int argv, char* argc[]){
+    FILE *pipe = popen("./printInUpper", "w");
+    if (pipe == NULL){
+        perror("failed to create pipe");
+        return -1;
+    }
+
+    int LEN = 0;
+    for(int i = 1; i < argv; ++i){
+        for(int j = 0; argc[i][j] != '\0'; ++j){
+            LEN = j;
+        }
+        fwrite(argc[i], sizeof(char), LEN + 1, pipe);
+        char spliter[1] = " ";
+        fwrite(spliter, sizeof(char), 1, pipe);
+    }
+    pclose(pipe);
+    return 0;
+}
