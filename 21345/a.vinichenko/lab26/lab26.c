5c6ef315e7cf2d05033bc174bfca79dc10622169
Alexander Vinichenko <a.vinichenko@g.nsu.ru>
2023-12-17 20:22:49 +0700

    Vinichenko lab26

---
+++ 21345/a.vinichenko/lab26/lab26.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define TEXT_SIZE 15
+int main()
+{
+    char line[TEXT_SIZE];
+
+    FILE* fpin = popen("echo Hello, World", "r");
+    FILE* fout = popen("tr [:lower:] [:upper:]", "w");
+
+    if(!fpin)
+    {
+        perror("Error popen");
+    }
+
+    if(!fout)
+    {
+        perror("Error popen");
+    }
+
+    while(fgets(line, TEXT_SIZE, fpin) != NULL)
+    {
+        if(ferror(fpin))
+        {
+            perror("Error read");
+            if(pclose(fpin))
+            {
+                perror("Error close");
+            }
+            return -1;
+        }
+        fputs(line, fout);
+    }
+
+    if(pclose(fpin) == - 1)
+    {
+        perror("Error close");
+        return -1;
+    }
+    if(pclose(fout) == -1)
+    {
+        perror("Error close");
+        return -1;
+    }
+
+}
