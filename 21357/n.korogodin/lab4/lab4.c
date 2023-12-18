ecc47e513229b50378342af6572449297879aab6
Nickolay_Korogodin <143979961+JetLuck12@users.noreply.github.com>
2023-12-18 10:12:42 +0700

    probel posle skobochki

--- 21357/n.korogodin/lab4/lab4.c
+++ 21357/n.korogodin/lab4/lab4.c
@@ -5,7 +5,7 @@
 typedef struct Node {
     char* data;
     struct Node* nextNode;
-}Node;
+} Node;
 
 Node* createNode() {
     Node* node = (Node*)malloc(sizeof(Node));
@@ -23,7 +23,7 @@ Node* setNodeData(char* str) {
 
     node->data = (char*)calloc((strlen(str) + 1), sizeof(char));
     if (node->data == NULL) {
-        perror("calloc error");
+        printf("calloc error");
         free(node);
         return NULL;
     }
@@ -77,7 +77,7 @@ int main() {
 
     Node* head = createNode();
     if (head == NULL) {
-        perror("couldnt malloc");
+        printf("couldnt malloc");
         return 1;
     }
     Node* currentNode = head;
@@ -90,7 +90,7 @@ int main() {
         currentNode->nextNode = setNodeData(buffer);
         if (currentNode->nextNode == NULL) {
             if (head == NULL) {
-                perror("couldnt malloc");
+                printf("couldnt malloc");
                 freeList(head);
                 return 1;
             }
