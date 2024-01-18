a731ecc19fa0d80c608971e7b836d41f7ed0ffe5
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2024-01-18 16:09:26 +0700

    Update server.c

--- 22213/a.shushakov1/server.c
+++ 22213/a.shushakov1/server.c
@@ -1,62 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <ctype.h>
-#include <string.h>
-#include <netinet/in.h>
 
-#define SERVER_SOCK_FILE "server.sock"
-#define MSGSIZE 20
-
+#define SOCKET_NAME "server.sock"
 
 int main() {
-	int fd, length, status;
-	struct sockaddr_un sock_addr;
-	char got_message[MSGSIZE];
-	memset(got_message, '\0', MSGSIZE);
+    int descriptor;
+    if ((descriptor = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("Cannot create the socket");
+        exit(EXIT_FAILURE);
+    }
 
-	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-		perror("Socket creation failure");
-		exit(EXIT_FAILURE);
-	}
+    struct sockaddr_un addr;
+    
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
 
-	memset(&sock_addr, 0, sizeof(sock_addr));
-	sock_addr.sun_family = AF_UNIX;
-	strcpy(sock_addr.sun_path, SERVER_SOCK_FILE);
-	
-	unlink(SERVER_SOCK_FILE);
-	if ((status = bind(fd, (struct sockaddr *)&sock_addr, sizeof(sock_addr))) < 0) {
-		perror("Binding failure");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(fd, 1) == -1) {
-		perror("Listening failure");
-	}
-	
-	int accepted;
-	if ((accepted = accept(fd, NULL, NULL)) == -1) {
-		perror("Accept failed");
-	}
-		
-	while ((length = read(accepted, got_message, MSGSIZE)) > 0) {
-		for (int i = 0; i < length; i++) {
-			got_message[i] = toupper(got_message[i]);
-		}
-		if ((write(1, got_message, length)) == -1) {
-			perror("Write failure");
-			exit(EXIT_FAILURE);
-		};
-	};
+    unlink(SOCKET_NAME);
 
-	if (length == -1) {
-		perror("Read failure");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(descriptor, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        close(descriptor);
+        perror("Binding failure");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket created\n");
 
-	close(accepted);
-	close(fd);
-	unlink(SERVER_SOCK_FILE);
-	exit(EXIT_SUCCESS);
+    if (listen(descriptor, 1) == -1) {
+        unlink(SOCKET_NAME);
+        close(descriptor);
+        perror("Listening failure");
+        exit(EXIT_FAILURE);
+    }
+    printf("Server started listening\n");
+
+    int accepted;
+    if ((accepted = accept(descriptor, NULL, NULL)) == -1) {
+        unlink(SOCKET_NAME);
+        perror("Accept failed");
+        close(descriptor);
+        exit(EXIT_FAILURE);
+    }
+
+    unlink(SOCKET_NAME);
+
+    printf("Server accepted a connection\n");
+
+    long actuallyReadBytes;
+    char buffer[BUFSIZ];
+    while ((actuallyReadBytes = read(accepted, buffer, BUFSIZ)) > 0) {
+        for (int i = 0; i < actuallyReadBytes; i++) {
+            buffer[i] = (char) toupper(buffer[i]);
+        }
+
+        if ((write(1, buffer, actuallyReadBytes)) == -1) {
+            perror("Write to buffer error!");
+            close(accepted);
+            close(descriptor);
+            unlink(SOCKET_NAME);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (actuallyReadBytes == -1) {
+        perror("Read failure");
+        close(accepted);
+        close(descriptor);
+        unlink(SOCKET_NAME);
+        exit(EXIT_FAILURE);
+    }
+    printf("Server finished its' work\n");
+
+    close(accepted);
+    close(descriptor);
+    unlink(SOCKET_NAME);
+    exit(EXIT_SUCCESS);
 }
