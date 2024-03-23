b5a16f2fa1df29c75deb24957dc362b8cec3f02e
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2024-03-23 13:31:37 +0700

    Update lab_3.c

--- 22213/a.shushakov1/lab_3/lab_3.c
+++ 22213/a.shushakov1/lab_3/lab_3.c
@@ -1,1 +1,29 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void fileWork(FILE* file){
+    if (file == NULL){
+        perror("Couldn't open file");
+        exit(1);
+    }
+    else{
+        fclose(file);
+    }
+}
+void idPrint(){
+    printf("User real id is %d\n", getuid());
+    printf("User effective is %d\n", geteuid());
+}
+int main(int argc, char* argv[]){
+    idPrint();
+    FILE* file = fopen("1.txt", "w");
+    fileWork(file);
+    if (setuid(getuid()) == -1){
+        perror("Couldn't change setuid");
+        exit(1);
+    }
+    idPrint();
+    file = fopen("1.txt", "w");
+    fileWork(file);
+    exit(0);
+}
